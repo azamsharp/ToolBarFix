89906249f7838ec37e5f45abf821862f43bb65cf
azamsharp <azamsharp@gmail.com>
2016-06-29 23:00:05 -0500

    Fixing the ToolBar Alignment Issue with Status Bar

---
+++ ToolBarFix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToolBarFix
+//
+//  Created by Mohammad Azam on 6/29/16.
+//  Copyright © 2016 Mohammad Azam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ToolBarFix/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  ToolBarFix
+//
+//  Created by Mohammad Azam on 6/29/16.
+//  Copyright © 2016 Mohammad Azam. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+    return UIStatusBarStyleLightContent;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
